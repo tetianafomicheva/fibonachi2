84e9c9c0cf01de739b0e154e87cad7fd2f380506
Tetiana <tetyana.fomscheva@mail.ru>
2016-12-16 14:15:07 +0000

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // JavaScript File
+function fib(n){
+  if (n <= 2) 
+  return 1;
+  
+  return fib(n - 1) + fib(n - 2);
+}
+var n = Number(prompt('Введіть число n'));
+var result = fib(n);
+document.write(result);
